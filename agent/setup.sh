8444f570609fd36921d5e70e5c430adff9a94f09
Erik Blomqvist <erikblomqvist3@gmail.com>
2023-03-02 11:20:26 +0000

    Add support for Apple Silicon

--- agent/setup.sh
+++ agent/setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+# This is an old version, but we know it works, so will not attempt an upgrade right now.
+version=2.284.0
+arch=$(/usr/bin/arch)
+# On Intel the arch is returned as i386, so we have to change it to x64.
+if [ $arch != arm64 ]; then
+	arch=x64
+fi
+file=actions-runner-osx-$arch-$version.tar.gz
 
 mkdir -p /Users/admin/agent
 cp connect.sh /Users/admin/agent/
 cp svc.sh /Users/admin/agent/
 cd /Users/admin/agent/
-curl -o actions-runner-osx-x64-2.284.0.tar.gz \
-	-L https://github.com/actions/runner/releases/download/v2.284.0/actions-runner-osx-x64-2.284.0.tar.gz
-tar xzf ./actions-runner-osx-x64-2.284.0.tar.gz
+
+curl -o $file \
+	-L https://github.com/actions/runner/releases/download/v$version/$file
+tar xzf ./$file
