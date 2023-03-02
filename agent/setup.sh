dde6f05f580a00c5bcafaf16459ec275b8fdf478
Erik Blomqvist <erikblomqvist3@gmail.com>
2023-03-02 14:24:06 +0000

    Update runner version

--- agent/setup.sh
+++ agent/setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # This is an old version, but we know it works, so will not attempt an upgrade right now.
-version=2.284.0
+version=2.302.1
 arch=$(/usr/bin/arch)
 # On Intel the arch is returned as i386, so we have to change it to x64.
 if [ $arch != arm64 ]; then
 	arch=x64
 fi
 file=actions-runner-osx-$arch-$version.tar.gz
+url=https://github.com/actions/runner/releases/download/v$version/$file
 
 mkdir -p /Users/admin/agent
 cp connect.sh /Users/admin/agent/
 cp svc.sh /Users/admin/agent/
 cd /Users/admin/agent/
 
-curl -o $file \
-	-L https://github.com/actions/runner/releases/download/v$version/$file
+curl -o $file -L $url 
 tar xzf ./$file
