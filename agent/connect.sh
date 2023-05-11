5f284469bca1d9f00ceabd9ee42c4eb92beb2c21
Mayur Kumar <102611744+mayur-kumar1@users.noreply.github.com>
2023-05-11 12:52:33 +0100

    fix: Add change as per comments

--- agent/connect.sh
+++ agent/connect.sh
@@ -1,20 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-RUNNER_APPLICATION_DIRECTORY=$HOME/
+RUNNER_APPLICATION_DIRECTORY=$HOME
 fetch_metadata() {
-    curl -s "http://169.254.169.254/metadata/$1" | jq -r .value
+    curl -s "http://169.254.169.254/metadata/$1" | \
+        python3 -c "import sys, json; print(json.load(sys.stdin)['value'])"
 }
 
 vm_name=`fetch_metadata orka_vm_name`
 user=`fetch_metadata github_user`
 repo=`fetch_metadata github_repo_name`
-pat=`get-github-pat`
+which get-github-pat
 if [ $? -eq 1 ]; then
-	echo 'get-github-pat exited with error code 1. trying to get pat from metadata'
-	unset pat
+	echo 'get-github-pat not found. Trying to get pat from metadata.'
 	pat=`fetch_metadata github_pat`
+else
+	pat=`get-github-pat`
 fi
-
 runner_token=$(curl \
 -XPOST \
 -H"Accept: application/vnd.github.v3+json" \
@@ -22,7 +23,7 @@ runner_token=$(curl \
 "https://api.github.com/repos/$user/$repo/actions/runners/registration-token" | \
         python3 -c "import sys, json; print(json.load(sys.stdin)['token'])")
 
-cd $RUNNER_APPLICATION_DIRECTORY
+cd $RUNNER_APPLICATION_DIRECTORY/agent
 echo "export GITHUB_TOKEN=$pat" >> $HOME/.bashrc
 ./config.sh --url $repo_url --token $runner_token --runnergroup "Default" --name $vm_name --work "_work" --labels $vm_name
 ./svc.sh install
--- agent/setup.sh
+++ agent/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This will always get the latest version of the runner binary.
-version=`curl -s https://api.github.com/repos/actions/runner/releases/latest | jq -r .tag_name | cut -c 2-`
+version=`curl -s https://api.github.com/repos/actions/runner/releases/latest | \
+        python3 -c "import sys, json; print(json.load(sys.stdin)['tag_name'])" | cut -c 2-`
 arch=$(/usr/bin/arch)
 # On Intel the arch is returned as i386, so we have to change it to x64.
 if [ $arch != arm64 ]; then
